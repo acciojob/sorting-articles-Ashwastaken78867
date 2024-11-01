a04c7b2fe64531ac4bfb96cf7a3258b23e917fb6
Bagda@Sh <168397146+Ashwastaken78867@users.noreply.github.com>
2024-11-01 11:59:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
-//your JS code here. If required.
-const bands = [
-    "The Rolling Stones",
-    "A Perfect Circle",
-    "The Beatles",
-    "An Endless Sporadic",
-    "Led Zeppelin"
-];
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-function strip(bandName) {
-    return bandName.replace(/^(a |an |the )/i, '').trim();
-}
 
-bands.sort((a, b) => strip(a).localeCompare(strip(b)));
+bands.sort((a, b) => a.replace(/^(a |the |an )/i, '').localeCompare(b.replace(/^(a |the |an )/i, '')));
 
-function displayBands() {
-    const ul = document.getElementById('band');
-    ul.innerHTML = '';
-
-    bands.forEach(band => {
-        const li = document.createElement('li');
-        li.textContent = band;
-        ul.appendChild(li);
-    });
-}
-
-displayBands();
+document.getElementById('bands').innerHTML = bands.map(band => `<li>${band}</li>`).join('');
