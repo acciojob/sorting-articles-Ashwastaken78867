4388689b47574ac0598391e308777a7bf6d66147
Bagda@Sh <168397146+Ashwastaken78867@users.noreply.github.com>
2024-11-05 17:31:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bands = [
+    'The Plot in You',
+    'The Devil Wears Prada',
+    'Pierce the Veil',
+    'Norma Jean',
+    'The Bled',
+    'Say Anything',
+    'The Midway State',
+    'We Came as Romans',
+    'Counterparts',
+    'Oh, Sleeper',
+    'A Skylit Drive',
+    'Anywhere But Here',
+    'An Old Dog'
+];
 
-
-bands.sort((a, b) => a.replace(/^(a |the |an )/i, '').localeCompare(b.replace(/^(a |the |an )/i, '')));
+bands.sort((a, b) => {
+    const bandA = a.replace(/^(a |an |the )/i, '').trim();
+    const bandB = b.replace(/^(a |an |the )/i, '').trim();
+    return bandA.localeCompare(bandB);
+});
 
 document.getElementById('bands').innerHTML = bands.map(band => `<li>${band}</li>`).join('');
